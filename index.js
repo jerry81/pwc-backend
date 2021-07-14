c2eba2b00df28f2b34e997be98691b94c872a773
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-14 13:46:08 +0800

    feat: attempt to resolve cors issues

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { client } from "./db.js";
+import cors from 'cors'
 
 const app = express();
+app.use(cors())
 const port = 3000;
 
 const pwc = client.db("sample_restaurants");
